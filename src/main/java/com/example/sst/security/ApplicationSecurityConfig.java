f0169a0e9198a274b329e2d15b5b66b4c0448b11
rfedorov <rodion.fedorov2018@gmail.com>
2022-10-16 21:11:48 +0300

    Cleanup

--- src/main/java/com/example/sst/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/sst/security/ApplicationSecurityConfig.java
@@ -2,7 +2,6 @@ package com.example.sst.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
